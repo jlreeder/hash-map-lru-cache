9c8d780741da66f87bdf3471acb9f41e10224992
Jason Reeder <info@jasonreeder.com>
2017-02-03 15:32:36 -0800

    Start HashMap

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,20 +2,31 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_reader :count
+  attr_reader :count, :num_buckets
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def include?(key)
+    hashed_key = key.hash
+    @store[hashed_key % num_buckets].include?(hashed_key)
   end
 
   def set(key, val)
+    hashed_key = key.hash
+    if include?(key)
+      @store[hashed_key % num_buckets].update(hashed_key, val)
+    else
+      @store[hashed_key % num_buckets].append(hashed_key, val)
+    end
   end
 
   def get(key)
+    hashed_key = key.hash
+    @store[hashed_key % num_buckets].get(hashed_key)
   end
 
   def delete(key)
@@ -36,10 +47,6 @@ class HashMap
   alias_method :[]=, :set
 
   private
-
-  def num_buckets
-    @store.length
-  end
 
   def resize!
   end
