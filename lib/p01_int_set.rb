c71bf1faec3ededd358b3b2ac4bbffedc5c19c87
Jason Reeder <info@jasonreeder.com>
2017-02-03 10:40:26 -0800

    add code for IntSet

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -29,26 +29,27 @@ end
 
 class IntSet
   def initialize(num_buckets = 20)
+    @num_buckets = num_buckets
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return if self.include?(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num) if self.include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
-
-  def num_buckets
-    @store.length
+    @store[num % @num_buckets]
   end
 end
 
