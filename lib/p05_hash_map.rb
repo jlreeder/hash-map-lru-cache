534dd6c0868ca48e3e6ef1824682d81ac8b90b13
Jason Reeder <info@jasonreeder.com>
2017-02-03 16:40:20 -0800

    Add resizegit add -A; increase count

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,7 +2,9 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_reader :count, :num_buckets
+  include Enumerable
+
+  attr_reader :count, :num_buckets, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -20,7 +22,9 @@ class HashMap
     if include?(key)
       @store[hashed_key % num_buckets].update(key, val)
     else
+      resize! if @count == @num_buckets
       @store[hashed_key % num_buckets].append(key, val)
+      @count += 1
     end
   end
 
@@ -32,6 +36,7 @@ class HashMap
   def delete(key)
     hashed_key = key.hash
     @store[hashed_key % num_buckets].remove(key)
+    @count -= 1
   end
 
   def each
@@ -58,10 +63,11 @@ class HashMap
   private
 
   def resize!
-  end
-
-  def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    new_hash = HashMap.new(@num_buckets * 2)
+    each do |k, v|
+      new_hash[k] = v
+    end
+    @store = new_hash.store
   end
 
 end
