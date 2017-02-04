728c1b78c72ad738e8d38f9f3cf771cb2d7e11b6
Jason Reeder <info@jasonreeder.com>
2017-02-03 16:41:18 -0800

    Uncomment #to_s

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -49,13 +49,12 @@ class HashMap
     @store
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
